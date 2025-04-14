a3012ec283cbac014004e7a52b0c136179c9a832
Allen Sun <52902629+3-aln@users.noreply.github.com>
2025-04-14 19:14:04 -0400

    Complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
+  for (let i = 0; i < array.length; i++) {
+    for (const itemToRemove of itemsToRemove) {
+      if (array[i] === itemToRemove) {
+        // Remove item i and shift the remaining arrays items to the left
+        array.splice(i, 1);
+        // Since the next item has been shifted into array index i, 
+        // check this same array index next iteration.
+        i--;
+      }
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
