52a913e7b2eb2ccd840d93dc3f0b0491f0aa215e
Allen Sun <52902629+3-aln@users.noreply.github.com>
2025-04-14 22:05:07 -0400

    Complete exercise 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  // Years divisible by 4
+  if (year % 4 == 0) {
+    // Years divisible by 100
+    if (year >= 100 && year % 100 == 0) {
+      // Years divisible by 400
+      if (year >= 400 && year % 400 == 0) {
+        return true;
+      }
+      return false;
+    }
+    return true;
+  }
+  // Years not divisible by 4
+  return false;
 };
 
 // Do not edit below this line
