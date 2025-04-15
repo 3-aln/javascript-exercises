01df4fd7467e10b47c892ca96cc22bce66456ad9
Allen Sun <52902629+3-aln@users.noreply.github.com>
2025-04-14 21:46:50 -0400

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+  // Disallow non-integers
+  if (!Number.isInteger(int1) || !Number.isInteger(int2)) return "ERROR";
+  // Disallow negative integers
+  if (int1 < 0 || int2 < 0) return "ERROR";
+  
+  // Identify which number is lower and which is higher
+  let lowerInt, upperInt;
+  if (int1 <= int2) {
+    lowerInt = int1;
+    upperInt = int2;
+  } else {
+    lowerInt = int2;
+    upperInt = int1;
+  }
 
+  // Sum numbers and return the result
+  let sum = 0;
+  for (let n = lowerInt; n <= upperInt; n++) {
+    sum += n;
+  }
+  return sum;
 };
 
 // Do not edit below this line
