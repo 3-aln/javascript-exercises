a86363ad912bfa54dd4b45128cc5bbb566ea094d
Allen Sun <52902629+3-aln@users.noreply.github.com>
2025-05-11 15:52:00 -0400

    Complete exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	return numArray.reduce((sum, number) => sum + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  return numArray.reduce((product, number) => product * number, 1);
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	return base ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+
+  // Keep a running product of numbers from 1 to the inputted number.
+  // 0! will not enter this loop - in that case the result correctly remains 1.
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
